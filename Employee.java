6d30a8326220b318e81b9a3a04bed12f8e0a5f64
aakashde <89719019+aakashde@users.noreply.github.com>
2021-08-29 12:17:07 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,29 @@
+package com.aakash.model;
+
+public class Employee {
+
+	public String firstName;
+	public String lastName;
+	
+	public Employee(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
